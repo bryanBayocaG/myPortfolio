fe67e7099af6db11d49c00b667a0ff62e3987e7c
bryanbayoca686 <bryanbayoca686@gmail.com>
2024-11-25 21:53:40 +0800

    border

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -15,15 +15,15 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden rounded-xl p-[1px] focus:outline-none"
+      className="relative inline-flex h-12 w-full md:w-60 md:mt-10 overflow-hidden p-[1px] focus:outline-none rounded-xl"
       onClick={handleClick}
     >
-      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#1d4ed8_0%,#010570_50%,#1d4ed8_100%)]" />
+      <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#1d4ed8_0%,#010570_50%,#1d4ed8_100%)] rounded-md " />
 
       {/* remove px-3 py-1, add px-5 gap-2 */}
       <span
-        className={`inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg
-             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 ${otherClasses}`}
+        className={`inline-flex h-full w-full cursor-pointer items-center justify-center 
+             bg-slate-950 px-7 text-sm font-medium text-white backdrop-blur-3xl gap-2 rounded-full ${otherClasses}`}
       >
         {position === "left" && icon}
         {title}
