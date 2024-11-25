d3c87acaab55ed4f2be2950784e506741cef0c33
bryanbayoca686 <bryanbayoca686@gmail.com>
2024-11-25 16:09:30 +0800

    up

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,14 +1,15 @@
+"use client";
 import { useState } from "react";
 import { IoCopyOutline } from "react-icons/io5";
 
 // Also install this npm i --save-dev @types/react-lottie
-import Lottie from "react-lottie";
+// import Lottie from "react-lottie";
 
 import { cn } from "@/lib/utils";
 
 import { BackgroundGradientAnimation } from "./GradientBg";
-import GridGlobe from "./Gridglobe";
-// import animationData from "@/data/confetti.json";
+import GridGlobe from "./GridGlobe";
+import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 
 export const BentoGrid = ({
@@ -56,14 +57,14 @@ export const BentoGridItem = ({
 
   const [copied, setCopied] = useState(false);
 
-  // const defaultOptions = {
-  //   loop: copied,
-  //   autoplay: copied,
-  //   animationData: animationData,
-  //   rendererSettings: {
-  //     preserveAspectRatio: "xMidYMid slice",
-  //   },
-  // };
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const handleCopy = () => {
     const text = "hsu@jsmastery.pro";
