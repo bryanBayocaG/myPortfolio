ade3b83ce2880161ac7525f0d1a2dcd119cb5b3a
bryanbayoca686 <bryanbayoca686@gmail.com>
2024-11-28 00:31:56 +0800

    no globe3

--- app/my_interests/page.tsx
+++ app/my_interests/page.tsx
@@ -1,17 +1,11 @@
-"use client";
-import { AnimatePresence } from "framer-motion";
 import React from "react";
-import { useSearchParams } from "next/navigation";
 
 function Page() {
-  const router = useSearchParams();
   return (
-    <AnimatePresence mode="wait">
-      <div>
-        <h1>hello</h1>
-        <p>hshdkaksdhjfkljahsdljh ahsdkjfhajsdhl</p>
-      </div>
-    </AnimatePresence>
+    <div>
+      <h1>hello</h1>
+      <p>hshdkaksdhjfkljahsdljh ahsdkjfhajsdhl</p>
+    </div>
   );
 }
 
