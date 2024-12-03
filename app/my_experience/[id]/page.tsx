0590cce5d1072740c3d8ebf16d624c4dcde80ef2
bryan <bryanbayoca686@gmail.com>
2024-12-03 19:27:43 +0800

    exp page

--- app/my_experience/[id]/page.tsx
+++ app/my_experience/[id]/page.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { navItems } from "@/data";
+import { Content } from './components/Content';
+import AllPageWrapper from '@/components/AllPageWrapper';
+import { FloatingNav } from '@/components/ui/FloatingNavbar';
 
 const page = ({ params }: any) => {
+
     return (
-        <div>
-            <h1>My exp</h1>
-            {params.id}
-        </div>
+        <AllPageWrapper>
+            <>
+
+                <FloatingNav navItems={navItems} />
+                <div className="h-20 " />
+                <Content />
+            </>
+        </AllPageWrapper>
     )
 }
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["assets.aceternity.com"], // Add your image hostnames here
+  },
+};
 
 export default nextConfig;
